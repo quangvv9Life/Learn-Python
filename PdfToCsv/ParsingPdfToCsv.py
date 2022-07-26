3f116cd15517f230c62f69df78761db6b3da65ab
DESKTOP-ESDPF32\Dell <vuvietquang90@gmail.com>
2022-07-26 18:46:44 +0700

    Study Dataframe

--- PdfToCsv/ParsingPdfToCsv.py
+++ PdfToCsv/ParsingPdfToCsv.py
@@ -1,18 +1,20 @@
 import tabula
-import pandas
+import pandas as pd
 
 PDFfilename2 = "VTN_FCT_2007.pdf"
 
-df = tabula.read_pdf(PDFfilename2, pages = 'all')[0:10]
+df = tabula.read_pdf(PDFfilename2, pages = 'all')
 
 # tabula.io.convert_into("VTN_FCT_2007.pdf","VTN_FCT_2007.csv", output_format=  "csv", pages = 'all')
 
+pd.DataFrame(df, columns=["colummn"])
 
+print(df)
 
 # df = PDFfilename2.encode('utf-8')
 
 # print(tabula.read_pdf(df))
-# print(type(df))
+print(type(df))
 # tabula.convert_into(df, "VTN_FCT_EN.csv", output_format=  "csv", pages = 'all')
 
-df.to_csv(r'C:\Users\Dell\Documents\12_MyfitnessPal\VTN_FCT_EN.csv', encoding='utf-8')
+df.to_csv(r'C:\Users\Dell\Documents\12_MyfitnessPal\VTN_FCT_EN.csv', encoding='utf-8', index=False)
