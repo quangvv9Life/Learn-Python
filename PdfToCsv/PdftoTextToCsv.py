7d0e4a48956a0bdf9471d5ee31152a44cf084590
DESKTOP-ESDPF32\Dell <vuvietquang90@gmail.com>
2022-07-28 22:47:04 +0700

    Extracted text is 2 pages ahead of extracted tables

--- PdfToCsv/PdftoTextToCsv.py
+++ PdfToCsv/PdftoTextToCsv.py
@@ -10,7 +10,7 @@ import io
 # from csv import writer
 def append_element_of_list_as_row(file_name, list_of_elem):
     # Open file in append mode
-    with open(file_name, 'a+') as write_obj:
+    with open(file_name, 'a+', encoding="utf-8") as write_obj:
         # Create a writer object from csv module
         csv_writer = csv.writer(write_obj)
         # Add contents of list as last row in the csv file
@@ -18,11 +18,11 @@ def append_element_of_list_as_row(file_name, list_of_elem):
 
 #Load your PDF
 with io.open(r"C:\Users\Dell\Documents\Python Test\PdfToCsv\VTN_FCT_2007.pdf", "rb") as f:
-#    pdf = pdftotext.PDF(f)
-    # pdf = tabula.read_pdf(f, pages = 'all')
-    # pdf = tabula.read_pdf(f, pages = 'all')[2]
-    # pagez = [14,15]
-    pagez = list(range(13,22+1))
+    # pagez = list(range(13,36+1))
+    # pagez = list(range(38,64+1))
+    # pagez = list(range(66,99+1))
+    # pagez = list(range(85,99+1))
+    pagez = list(range(97,99+1))
     pdf = tabula.read_pdf(f, pages = pagez)
     pdfReader = PyPDF2.PdfFileReader(f) 
     output = []
@@ -36,9 +36,19 @@ for i in range(len(output)):
     Listz.append(innerList[0])
 
 with open("Extract.csv", "a") as file1:
-    for i in range(len(pagez)):
+    # for i in range(len(pagez)):%
+    for i, j in enumerate(pagez):
+    # for i in pagez:
         append_element_of_list_as_row("Extract.csv", Listz[i])
-        pdf[i].to_csv('Extract.csv', mode = "a") #, sep = ',', mode='a', index= False,header=False)
+        # Corresponding tables are behind by 2 pages
+        try:
+
+            pdf[i+2].to_csv('Extract.csv', mode = "a") #, sep = ',', mode='a', index= False,header=False)
+        except AttributeError:
+            break
+            
+        
+        
 
 # print(Listz)
 # print(pdf)
