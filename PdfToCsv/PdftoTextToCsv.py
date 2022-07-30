5f13418c3acb045290f76446f31499bc3f01eafc
DESKTOP-ESDPF32\Dell <vuvietquang90@gmail.com>
2022-07-30 09:34:37 +0700

    Extract ingredient's code from text using regex partern

--- PdfToCsv/PdftoTextToCsv.py
+++ PdfToCsv/PdftoTextToCsv.py
@@ -4,6 +4,7 @@ import PyPDF2
 import tabula
 import pandas as pd
 import io
+import re
 
 # Define a function called "append data to csv"
 
@@ -16,14 +17,28 @@ def append_element_of_list_as_row(file_name, list_of_elem):
         # Add contents of list as last row in the csv file
         csv_writer.writerow(list_of_elem)
 
-#Load your PDF
+# Dell Desktop : Load your PDF
+# with io.open(r"G:\tryPython\Python Test\PdfToCsv\VTN_FCT_2007.pdf", "rb") as f:
+# Dell Laptop : Load your PDF
 with io.open(r"C:\Users\Dell\Documents\Python Test\PdfToCsv\VTN_FCT_2007.pdf", "rb") as f:
-    # pagez = list(range(13,36+1))
+    pagez = list(range(13,36+1))
     # pagez = list(range(38,64+1))
     # pagez = list(range(66,99+1))
     # pagez = list(range(85,99+1))
-    pagez = list(range(97,99+1))
-    pdf = tabula.read_pdf(f, pages = pagez)
+    # pagez = list(range(97,99+1))
+    # pagez = 98
+    # pagez = list(range(102,228+1))
+    # pagez = list(range(231,286+1))
+    # pagez = list(range(288,302+1))
+    # pagez = list(range(305,386+1))
+    
+    #No need encode - majority
+    try:
+        pdf = tabula.read_pdf(f, pages = pagez)
+    except UnicodeDecodeError:
+    #Need encode - minority
+        pdf = tabula.read_pdf(f, pages = pagez, encoding = "ISO-8859-1")
+ 
     pdfReader = PyPDF2.PdfFileReader(f) 
     output = []
     for i in pagez:
@@ -31,58 +46,25 @@ with io.open(r"C:\Users\Dell\Documents\Python Test\PdfToCsv\VTN_FCT_2007.pdf", "
         output.append(pageObj.extractText())
 
 Listz = []
+patern = r'(?:sè:)+\d+'
+patern1 = r'(?:sè:)+\\n\d+'
 for i in range(len(output)):
-    innerList = output[i].split("\n")
-    Listz.append(innerList[0])
+    # innerList = output[i].split("\n")
+    innerList = re.findall(patern,output[i])
+    if innerList == []:     
+        innerList = re.findall(patern1, output[i])
+    try:
+        Listz.append(innerList[0])
+    except IndexError:
+        break
 
-with open("Extract.csv", "a") as file1:
+with open("Extract_1.csv", "a") as file1:
     # for i in range(len(pagez)):%
     for i, j in enumerate(pagez):
     # for i in pagez:
-        append_element_of_list_as_row("Extract.csv", Listz[i])
+        append_element_of_list_as_row("Extract_1.csv", Listz[i])
         # Corresponding tables are behind by 2 pages
         try:
-            pdf[i+2].to_csv('Extract.csv', mode = "a") #, sep = ',', mode='a', index= False,header=False)
-        except AttributeError:
+            pdf[i+2].to_csv('Extract_1.csv', mode = "a") #, sep = ',', mode='a', index= False,header=False)
+        except IndexError:
             break
-            
-        
-        
-
-# print(Listz)
-# print(pdf)
-# print(type(Listz))
-# print(type(pdf))
-# Save all text to a txt file.
-
-# for i in range(len(pdf)):
-#     df = pd.DataFrame(pdf[i])
-#     header_row = df.iloc[0]
-#     df2 = pd.DataFrame(df.values[1:], columns=header_row)
-
-#     print(df2)
-# append_element_of_list_as_row("Extract.csv", Listz)
-
-# with open('VTN_FCT.txt', 'w', encoding="utf-8") as f:
-
-#     pd.concat(pdf).to_csv(f)
-    # f.write("\n\n".join(pdf))
-    # dfAsString = pdf.to_string(header=False, index=False)
-    # f.write(dfAsString)
-
-# save_path = "C:\\Users\\Dell\\Documents\\Python Test\\PdfToCsv\\"
-
-# completeName_in = os.path.join(save_path, 'crimestory' + '.txt')
-# completeName_out = os.path.join(save_path, 'crimestoryycsv' + '.csv')
-
-# file1 = open(completeName_in)
-# In_text = csv.reader(file1, delimiter=',')
-
-# file2 = open(completeName_out, 'w')
-# out_csv = csv.writer(file2)
-
-# file3 = out_csv.writerows(In_text)
-
-# file1.close()
-# file2.close()
-
